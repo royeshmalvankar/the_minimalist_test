f557eb0aee485905dace99340e930d2716f0e243
royeshmalvankar <royeshmalvankar@gmail.com>
2024-08-31 10:51:09 +0530

    adding

---
+++ src/component/home.jsx
@@ -0,0 +1,23 @@
+import "../App.css"
+import image from "../assets/logo.png"
+const Home = () => {
+
+    return (    
+        <div className="background">
+            <div className="frame6">
+                <div className="rectangle1">
+                    <div className="rectangle2">
+                        <div className="topbar">
+                            <div className="logo">
+                                    <img  src={image} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
